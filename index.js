e5d5e339484a4785993769fb5ab8493081a4f618
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-03-20 01:21:29 +0000

    CSV parsing of substitutions Fixes #2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+
+let substitutions;
+
 let [program, startIn, substitutionFilename, targetDirectory] = process.argv;
 
 if (substitutionFilename == undefined || targetDirectory == undefined) {
@@ -5,4 +10,29 @@ if (substitutionFilename == undefined || targetDirectory == undefined) {
     "Invalid parameters, requires node . [substitutionfilename] [target directory]"
   );
   return;
+}
+
+//Get substitutions
+try {
+  substitutions = fs.readFileSync(substitutionFilename);
+} catch (error) {
+  console.error(`ERROR locating the substitutions file`);
+  return;
+}
+
+//Parse substitutions
+try {
+  const input = substitutions;
+
+  parse(
+    input,
+    {
+      comment: "#",
+    },
+    function (err, output) {
+      console.debug(output);
+    }
+  );
+} catch (error) {
+  console.error(`ERROR parsing the substitutions file`);
 }
