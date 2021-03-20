25b735f5c1a4bb78b028128db4ee2ef367f86205
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-03-20 18:30:20 +0000

    Iterate through files and replace substitutions Fixes #3

--- README.md
+++ README.md
@@ -5,19 +5,22 @@ Given a list of substitutions, replace all instances of each substitution target
 ### Built With
 
 This section should list any major frameworks that you built your project using. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
-* [Node.js](nodejs.org)
+
+- [Node.js](nodejs.org)
 
 ## Getting Started
 
-1. ```npm i``` to install the project dependencies
+1. `npm i` to install the project dependencies
 2. That's it!
 
 <!-- USAGE EXAMPLES -->
+
 ## Usage
 
-```node . [substitution file] [target directory]```
+`node . [substitution file] [target directory]`
 
 The substitution file should look like this:
+
 ```csv
 before,after
 target,replacement
@@ -27,7 +30,11 @@ apple,banana
 
 The program will go through each file in the target directory and rename every instance of each target value with it's corrosponding replacement value.
 
+You can try the example replacement with:
+`node . .\example\subs.csv .\example\target\`
+
 <!-- CONTRIBUTING -->
+
 ## Contributing
 
 Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
@@ -39,7 +46,8 @@ Contributions are what make the open source community such an amazing place to b
 5. Open a Pull Request
 
 <!-- ACKNOWLEDGEMENTS -->
-## Acknowledgements
-* [Choose an Open Source License](https://choosealicense.com)
-* [NodeCSV](https://github.com/adaltas/node-csv)
 
+## Acknowledgements
+
+- [Choose an Open Source License](https://choosealicense.com)
+- [NodeCSV](https://github.com/adaltas/node-csv)
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (substitutionFilename == undefined || targetDirectory == undefined) {
 try {
   substitutions = fs.readFileSync(substitutionFilename);
 } catch (error) {
-  console.error(`ERROR locating the substitutions file`);
+  console.error(`ERROR locating the substitutions file: ${error}`);
   return;
 }
 
@@ -30,9 +30,53 @@ try {
       comment: "#",
     },
     function (err, output) {
-      console.debug(output);
+      substitutions = output;
+      console.log(
+        `${substitutions.length} substitutions instructions found in ${substitutionFilename}`
+      );
+      getFiles();
     }
   );
 } catch (error) {
-  console.error(`ERROR parsing the substitutions file`);
+  console.error(`ERROR parsing the substitutions file: ${error}`);
+  return;
+}
+
+function getFiles() {
+  try {
+    let files = fs.readdirSync(targetDirectory);
+    console.log(
+      `${files.length} files found in target directory ${targetDirectory}`
+    );
+    replaceInFiles(files);
+  } catch (error) {
+    console.error(`ERROR Getting files from target directory: ${error}`);
+    return;
+  }
+}
+
+function replaceInFiles(fileNames) {
+  fileNames.forEach((fileName) => {
+    fs.readFile(`${targetDirectory}${fileName}`, {}, (error, data) => {
+      if (error != null) {
+        console.error(`Error reading file: ${filename}\nError: ${error}`);
+        return;
+      }
+
+      data = data.toString();
+
+      substitutions.forEach((substitution) => {
+        let [target, replacement] = substitution;
+        data = data.replace(target, replacement);
+      });
+
+      fs.writeFile(`${targetDirectory}${fileName}`, data, (error) => {
+        if (error != null) {
+          console.error(`Error writing to file: ${filename}\nError: ${error}`);
+          return;
+        }
+        console.log(`${fileName} replacements completed!`);
+      });
+    });
+  });
 }
